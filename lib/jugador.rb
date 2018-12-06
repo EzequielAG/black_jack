0f519f41e2a24af19c79793e35f037241b5cfe35
Eze <eze94ale@gmail.com>
2018-12-06 15:54:25 -0300

    test de pedir cartas

---
+++ lib/jugador.rb
@@ -0,0 +1,22 @@
+class Jugador
+
+	def initialize
+		@puntaje = 0
+		@carta1 = 0
+	end
+
+	def puntaje
+		return @puntaje
+	end
+
+	def pedirCarta
+		if @carta1 > 0
+			@puntaje += @carta1
+		end
+	end
+
+	def proximaCarta valor
+		@carta1 = valor
+	end
+
+end
